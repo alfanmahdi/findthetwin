2997549e26bb506a6d9d4e3bc4f4544eb0209539
Gesang Widigdo <gesangwidigdo@gmail.com>
2023-12-09 21:48:07 +0700

    feat: Add Panel To Choose Difficulty

--- src/com/ftt/LevelPanel.java
+++ src/com/ftt/LevelPanel.java
@@ -14,40 +14,36 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
-public class MainMenuPanel extends JPanel {
+public class LevelPanel extends JPanel {
 	Color backgroundColor = Color.decode("#23253F");
-	public MainMenuPanel() {
-        setBorder(new EmptyBorder(10, 10, 10, 10));
-        setLayout(new GridBagLayout());
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.anchor = GridBagConstraints.NORTH;
-
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        
-        Dimension buttonSize = new Dimension(150, 50);
-
-        JPanel buttons = new JPanel(new GridBagLayout());
-        
-        addButton(buttons, "Play", gbc, buttonSize);
-        addButton(buttons, "Setting", gbc, buttonSize);
-        addButton(buttons, "Exit", gbc, buttonSize);
-
-        gbc.weighty = 1;
-        add(buttons, gbc);
-    }
+	public LevelPanel() {
+		setBorder(new EmptyBorder(10, 10, 10, 10));
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.anchor = GridBagConstraints.NORTH;
+		
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		
+		Dimension buttonSize = new Dimension(300, 50);
+		
+		JPanel buttons = new JPanel(new GridBagLayout());
+		
+		addButton(buttons, "Easy", gbc, buttonSize);
+		addButton(buttons, "Medium", gbc, buttonSize);
+		addButton(buttons, "Hard", gbc, buttonSize);
+		addButton(buttons, "Back to Main Menu", gbc, buttonSize);
+		
+		gbc.weighty = 1;
+		add(buttons, gbc);
+	}
 	
 	private void addButton(JPanel panel, String text, GridBagConstraints gbc, Dimension preferredSize) {
 		JButton button = new JButton(text);
@@ -66,25 +62,22 @@ public class MainMenuPanel extends JPanel {
 		button.addActionListener(new ButtonHandler());
 		
 		button.addMouseListener(new MouseAdapter() {
-        	public void mouseEntered(MouseEvent e) {
-        		button.setBackground(Color.WHITE);
-        		button.setForeground(backgroundColor);
-        	}
-        	
-        	public void mouseExited(MouseEvent e) {
-        		button.setBackground(backgroundColor);
-        		button.setForeground(Color.WHITE);
-        	}
-        	
+			public void mouseEntered(MouseEvent e) {
+				button.setBackground(Color.WHITE);
+				button.setForeground(backgroundColor);
+			}
+			
+			public void mouseExited(MouseEvent e) {
+				button.setBackground(backgroundColor);
+				button.setForeground(Color.WHITE);
+			}
 		});
 		
 		button.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		button.setFocusPainted(false);
+		button.setFocusable(false);
 		
 		panel.add(button, gbc);
 	}
-	
-	
 	
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -95,11 +88,11 @@ public class MainMenuPanel extends JPanel {
 		Dimension d = this.getSize();
 		
 		// Customize main menu appearance
-		Font fnt0 = new Font("arial", Font.BOLD, 96);
+		Font fnt0 = new Font("Arial", Font.BOLD, 62);
 		g.setFont(fnt0);
 		g.setColor(Color.WHITE);
-//		g.drawString("FIND THE TWIN", getWidth() / 2 - 350, getHeight() / 2 - 300);
-		centeredString("FIND THE TWIN", d.width, d.height, g);
+		
+		centeredString("Select Difficulty", d.width, d.height, g);
 	}
 	
 	public void centeredString(String s, int width, int height, Graphics g) {
@@ -113,26 +106,16 @@ public class MainMenuPanel extends JPanel {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			if (e.getActionCommand().equals("Play")) {
-				switchToPanel("Level");
-			} else if (e.getActionCommand().equals("Setting")) {
-				//
-			} else if (e.getActionCommand().equals("Exit")) {
-				JOptionPane option = new JOptionPane();
-				int choose = JOptionPane.showConfirmDialog(null, "Confirm exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
-				if (choose == JOptionPane.YES_OPTION) {
-					System.exit(0);
-				}
-				
+			if (e.getActionCommand().equals("Easy")) {
+				switchToPanel("InGame");
+			} else if (e.getActionCommand().equals("Back to Main Menu")) {
+				switchToPanel("MainMenu");
 			}
-			
 		}
-		
 	}
 	
 	private void switchToPanel(String panelName) {
 		GamePanel parent = (GamePanel) getParent();
 		parent.showPanel(panelName);
 	}
-	
 }
